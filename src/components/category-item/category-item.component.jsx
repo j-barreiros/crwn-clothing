bc80a654f2776b7af2ff7dfda6fb1f5fc02e846a
j-novaes <joaovictor.nb21@hotmail.com>
2023-06-26 12:02:47 +0100

    class 79

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,18 @@
+import "./category-item.styles.scss";
+
+const CategoryItem = ({category}) => {
+    const {title, imageUrl} = category;
+    return (
+        <article className="category-container">
+          <div className="background-image"
+            style={{backgroundImage:`url(${imageUrl})`}}
+          />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>SHOP NOW</p>
+          </div>
+        </article>
+      )
+}
+
+export default CategoryItem;
