325e05f4c2f8e0f452e8edcc069637d43459b469
j-novaes <joaovictor.nb21@hotmail.com>
2023-06-26 23:05:18 +0100

    class 87

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
 // React Router
-import { Route, Routes, Outlet } from "react-router";
+import { Route, Routes} from "react-router";
+
 // Routes
 import Home from "./routes/home/home.component";
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  )
-}
+import Navigation from "./routes/navigation/navigation.component";
 
 const Shop = () => {
   return (
-    <div>
-      <h1>Shop</h1>
-    </div>
+    <h1>SHOP</h1>
   )
 }
+
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
+        <Route path="shop" element={<Shop />}/>
       </Route>
     </Routes>
   );
