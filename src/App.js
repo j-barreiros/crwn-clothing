03c1f98f7cc9d0f894be156fd7b74ba9db8e7a88
j-novaes <joaovictor.nb21@hotmail.com>
2023-06-26 11:00:52 +0100

    class 76

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 
 const App = () => {
   return (
-    <div>
-      <h1>Crown Clothing</h1>
-    </div>
+    <section className="categories-container">
+
+      <article className="category-container">
+        <div className="cateogry-body-container">
+          <h2>Hats</h2>
+          <p>SHOP NOW</p>
+        </div>
+      </article>
+
+      <article className="category-container">
+        <div className="cateogry-body-container">
+          <h2>Jackets</h2>
+          <p>SHOP NOW</p>
+        </div>
+      </article>
+
+      <article className="category-container">
+        <div className="cateogry-body-container">
+          <h2>Sneakers</h2>
+          <p>SHOP NOW</p>
+        </div>
+      </article>
+
+      <article className="category-container">
+        <div className="cateogry-body-container">
+          <h2>Womens</h2>
+          <p>SHOP NOW</p>
+        </div>
+      </article>
+
+      <article className="category-container">
+        <div className="cateogry-body-container">
+          <h2>Mens</h2>
+          <p>SHOP NOW</p>
+        </div>
+      </article>
+
+    </section>
   );
 }
 
