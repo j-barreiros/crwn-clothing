c60d80d22ccaa2af4013eb6f879ce3fbe57fdba1
j-novaes <joaovictor.nb21@hotmail.com>
2023-07-16 11:03:14 +0100

    class 113

--- src/App.js
+++ src/App.js
@@ -5,12 +5,7 @@ import { Route, Routes} from "react-router";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/auth/authentication.component";
-
-const Shop = () => {
-  return (
-    <h1>SHOP</h1>
-  )
-}
+import Shop from "./routes/shop/shop.component";
 
 
 const App = () => {
